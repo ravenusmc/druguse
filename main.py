26e8f70e8d4782182fddc5c6f5cdc777b2a5828e
ravenusmc <mcuddy77@gmail.com>
2016-09-08 14:21:39 -0400

    changed file structure and added a file

---
+++ main.py
@@ -0,0 +1,10 @@
+#This is the main file of the project. 
+#Importing the libraries that will be used:
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from mainClass import *
+
+main = Main()
+main.welcome()
---
+++ mainClass.py
@@ -0,0 +1,11 @@
+class Main():
+
+  def welcome(self):
+    print("\033c")
+    print("---------~~~~----------")
+    print("--Welcome to Drug Use--")
+    print("----~~~---------~~~----")
+
+
+main = Main()
+main.welcome()
